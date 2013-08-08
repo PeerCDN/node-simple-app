4612e43455b1f65935e05816fb6dbe292bfc2bd4
Abi Raja <abimanyuraja@gmail.com>
2013-08-07 23:10:13 -0700

    Allow subclass to create its own routes and add middleware to the Express server

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+var debug = require('debug')('test-app')
 var path = require('path')
 var SimpleApp = require('./lib/SimpleApp')
 
 function App () {
   var self = this
 
-  SimpleApp.call(self, {
+  var opts = {
     name: 'Simple App',
     port: 9000,
     dbPath: path.join(__dirname, './app-db'),
     cookieSecret: 'secret'
-  })
+  }
+
+  // Call the parent constructor
+  SimpleApp.call(self, opts, self.init.bind(self))
 }
 App.prototype = Object.create(SimpleApp.prototype)
+
+/* Setup app-specific routes and middleware */
+App.prototype.init = function () {
+  var self = this
+  var app = self.app
+
+  app.get('/hello', function (req, res) {
+    res.end('Hello.')
+  })
+}
 
 /* Start the app */
 new App()
